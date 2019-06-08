b912c41eeaccbb7a1990aa3e189b1d8901a97ae2
faiyazuddeen <m.faiyaz123@gmail.com>
2019-06-08 18:26:11 +0530

    brought material design and material icons into core module from shared libs

--- apps/campusrope/src/app/app.module.ts
+++ apps/campusrope/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { AngularMaterialModule } from '@campusrope/material';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AngularMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
