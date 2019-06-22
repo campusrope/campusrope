9b19f5309197a2933a439e657f984e6fda132e19
faiyazuddeen <m.faiyaz123@gmail.com>
2019-06-22 14:16:14 +0530

    app level routing and sub level routing fixed

--- apps/campusrope/src/app/app.routes.ts
+++ apps/campusrope/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './core';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: LayoutComponent },
+  { path: '', pathMatch:'full', loadChildren: () =>
+  import('./core')
+  
+.then(m => m.CoreModule)},
   {
     path: 'login',
     loadChildren: () =>
       import('@campusrope/pages/login')
-      
+
   .then(m => m.PagesLoginModule)
   },
 ];
--- apps/campusrope/src/app/core/core.routes.ts
+++ apps/campusrope/src/app/core/core.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 import { HomeCenterMenuComponent } from './components/home-center-menu/home-center-menu.component';
 
-export const CORE_ROUTES: Routes = [
-  { path: '', pathMatch: 'full', component: HomeCenterMenuComponent,}
+const CENTER_PANEL_ROUTES:Routes = [
+  {path:'',pathMatch:'full',component:HomeCenterMenuComponent}
 ];
+
+
+export const CORE_ROUTES: Routes = [
+  { path: '', pathMatch: 'full', component: LayoutComponent,children: CENTER_PANEL_ROUTES}
+];
+
