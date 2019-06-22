fb953d135f29f6254819c988f06a3f11cea99d42
faiyazuddeen <m.faiyaz123@gmail.com>
2019-06-22 17:41:19 +0530

    implemented swipe action

--- apps/campusrope/src/app/core/core.routes.ts
+++ apps/campusrope/src/app/core/core.routes.ts
@@ -3,7 +3,7 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { HomeCenterMenuComponent } from './components/home-center-menu/home-center-menu.component';
 
 const CENTER_PANEL_ROUTES: Routes = [
-  { path: '', component: HomeCenterMenuComponent },
+  { path: '', component: HomeCenterMenuComponent, },
   {
     path: 'helpline',
     loadChildren: () =>
