72412af274b550b293e9bc8da4afc1376912e0f0
umarfarooq95 <mohamedumar931@gmail.com>
2019-06-23 21:12:50 +0530

    added admin list ui

--- libs/pages/admin/src/lib/admin-list/admin-list.component.ts
+++ libs/pages/admin/src/lib/admin-list/admin-list.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+const ELEMENT_DATA  = [
+  {name: 'Hydrogen', symbol: 'H'},
+  { name: 'Helium',  symbol: 'He'},
+  {name: 'Lithium',  symbol: 'Li'},
+  { name: 'Beryllium', symbol: 'Be'},
+  { name: 'Boron', symbol: 'B'},
+  { name: 'Carbon',  symbol: 'C'},
+  { name: 'Nitrogen',  symbol: 'N'},
+  { name: 'Oxygen',  symbol: 'O'},
+  { name: 'Fluorine', symbol: 'F'},
+  { name: 'Neon', symbol: 'Ne'},
+  
+];
+
 @Component({
   selector: 'campusrope-admin-list',
   templateUrl: './admin-list.component.html',
@@ -9,4 +23,8 @@ export class AdminListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  displayedColumns: string[] = ['position', 'Admins', 'Control System'];
+  dataSource = ELEMENT_DATA;
+
 }
--- libs/pages/admin/src/lib/pages-admin.module.ts
+++ libs/pages/admin/src/lib/pages-admin.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AngularMaterialModule } from '@campusrope/material';
 import { AdminListComponent } from './admin-list/admin-list.component';
 import { AdminAddComponent } from './admin-add/admin-add.component';
 import { AdminEditComponent } from './admin-edit/admin-edit.component';
-import { RouterModule } from '@angular/router';
 import { ADMIN_ROUTES } from './pages-admin.routes';
 
 @NgModule({
-  imports: [CommonModule,RouterModule.forChild(ADMIN_ROUTES)],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    RouterModule.forChild(ADMIN_ROUTES)
+  ],
   declarations: [
     AdminListComponent,
     AdminAddComponent,
