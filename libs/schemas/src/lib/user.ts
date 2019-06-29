562ba77599c278252ce0f54075b2c57a5936fe30
faiyazuddeen <m.faiyaz123@gmail.com>
2019-06-29 11:12:35 +0530

    user schema

--- libs/schemas/src/lib/user.ts
+++ libs/schemas/src/lib/user.ts
@@ -1,8 +1,22 @@
+import { User } from '@campusrope/models';
 import * as mongoose from 'mongoose';
 
-export const UserSchema = new mongoose.Schema({
-    name:String,
-    picture:String,
-    email:String,
-    google: String,
-},{ timestamps: true });
+export interface UserEntity extends mongoose.Document, User {
+  _id: string;
+}
+
+const schema = new mongoose.Schema(
+  {
+    _id: String,
+    email: { type: String, unique: true, required: true, index: true },
+    emailVerified: Boolean,
+    name: String,
+    pictureUrl: String,
+    phoneNumber: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserContext = mongoose.model<UserEntity>('User', schema);
