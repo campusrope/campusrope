136ffcd67f027d406c5ab16a591fa448aac05469
umarfarooq95 <mohamedumar931@gmail.com>
2019-06-30 16:17:21 +0530

    added accodion for helpline

--- libs/pages/helpline/src/lib/pages-helpline.module.ts
+++ libs/pages/helpline/src/lib/pages-helpline.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelplineListComponent } from './helpline-list/helpline-list.component';
 import { RouterModule } from '@angular/router';
+import { AngularMaterialModule } from '@campusrope/material';
 import { HELPLINE_ROUTES } from './pages-helpline.routes';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(HELPLINE_ROUTES)],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    RouterModule.forChild(HELPLINE_ROUTES)
+  ],
   declarations: [HelplineListComponent],
   exports: [HelplineListComponent],
   entryComponents: [HelplineListComponent]
