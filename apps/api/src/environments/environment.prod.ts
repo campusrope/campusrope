07a8947c0ca2f3019f423313506863d2fc4ae7af
faiyazuddeen <m.faiyaz123@gmail.com>
2019-07-05 16:44:37 +0530

    removed api module

--- apps/api/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { IConfig } from './config';
-
-export const environment:IConfig = {
-  production: true,
-  db:'mongodb://campusrope:uniteall1@ds145053.mlab.com:45053/campusrope',
-  addAuthorization: true,
-  verbose: true,
-  verboseApi: true,
-};
