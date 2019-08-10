6540a9d8f432e104ebc39050e78d24095295af58
umarfarooq95 <mohamedumar931@gmail.com>
2019-08-10 22:35:34 +0530

    implement helpline edit ui

--- src/app/features/helpline/helpline-edit/helpline-edit.component.ts
+++ src/app/features/helpline/helpline-edit/helpline-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { LocationService } from "src/app/core/location/location.service";
 
 @Component({
   selector: "app-helpline-edit",
@@ -7,9 +9,39 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HelplineEditComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+  titleAlert = "This field is required";
+
+  constructor(
+    private locationService: LocationService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      headline: [null, Validators.required],
+      description: [null, Validators.required],
+      headlineNumber: [null, Validators.required],
+      websiteLink: [null, Validators.required],
+      twitterLink: [null, Validators.required],
+      facebookLink: [null, Validators.required],
+      fileOnlineComplaintLink: "",
+      instagramLink: "",
+      whatsappLink: ""
+    });
+  }
+
+  goBack(): any {
+    this.locationService.goBack();
+  }
+
+  onHeadlineUpdate(): any {
+    if (!this.formGroup.valid) { return; }
+    console.log(this.formGroup.value);
   }
 
 }
