715fa2f1620e520e9cc1451b3451d6f7ce3783e6
umarfarooq95 <mohamedumar931@gmail.com>
2019-08-25 18:52:15 +0530

    implement call functionality for helpline phone number

--- src/app/features/helpline/links-dialog-modal/links-dialog-modal.component.ts
+++ src/app/features/helpline/links-dialog-modal/links-dialog-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import { NotificationService } from "src/app/core/core.module";
+import { Observable } from "rxjs";
+import { Store, select } from "@ngrx/store";
+import { AppState, getIsMobile } from "src/app/state";
 
 @Component({
   selector: "app-links-dialog-modal",
@@ -8,14 +11,17 @@ import { NotificationService } from "src/app/core/core.module";
   styleUrls: ["./links-dialog-modal.component.scss"]
 })
 export class LinksDialogModalComponent implements OnInit {
+  isMobile$: Observable<boolean>;
 
   constructor(
     public dialogRef: MatDialogRef<LinksDialogModalComponent>,
     private notificationService: NotificationService,
+    private store: Store<AppState>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   ngOnInit() {
+    this.isMobile$ = this.store.pipe(select(getIsMobile));
   }
 
   onCancelClick(): void {
@@ -35,5 +41,9 @@ export class LinksDialogModalComponent implements OnInit {
       document.execCommand("copy");
       document.body.removeChild(selBox);
       this.notificationService.success("Text Copied");
+    }
+
+    getPhoneNo(val: string) {
+      return `tel:${val}`;
     }
 }
